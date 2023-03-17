d84c416ac90d363bc77a7071b8db2db42d0d5aa5
Jitarciuc Ivan <ijitarciuc@T5PT421.AUSYNET.FR>
2023-03-17 15:56:04 +0200

    Clasic polimorf

---
+++ Cpp17/Polimorf.h
@@ -0,0 +1,53 @@
+class container_of_ints 
+{
+public:
+    virtual int size() const = 0;
+    virtual int& at(int) = 0;
+};
+
+
+
+class array_of_ints : public container_of_ints 
+{
+    int data[10] = {};
+public:
+    int size() const { return 10; }
+    int& at(int i) { return data[i]; }
+};
+
+
+class list_of_ints : public container_of_ints
+{
+    struct node
+    {
+        int data;
+        node *next;
+    };
+
+    node *head_ = nullptr;
+    int size_ = 0;
+   public:
+    int size() const { return size_; }
+    int& at(int i)
+    {
+        if (i >= size_) throw std::out_of_range("at");
+        node *p = head_;
+        for(int j=0; j < i; ++j)
+        {
+            p = p->next;
+        }
+
+        return p->data;
+    }
+
+    ~list_of_ints(){};
+};
+
+
+void double_each_elemnt( container_of_ints& arr)
+{
+    for(int i = 0; i< arr.size(); i++)
+    {
+        arr.at(i) *= 2;
+    }
+}
---
+++ Cpp17/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include "Polimorf.h"
+
+
+
+
+
+int main()
+{
+
+    array_of_ints arr;
+    double_each_elemnt(arr);
+
+    list_of_ints lst;
+    double_each_elemnt(lst);
+
+    std::cout<< "Stop!" << std::endl;
+    return 0;
+}
